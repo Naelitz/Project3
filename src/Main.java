0a3acb4b1ae2cd273111ab8a319a38a3f7fd307e
Naelitz <davidnaelitz@ymail.com>
2015-12-05 00:17:01 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 public class Main
 {
 	static int[] numArray; // Will hold the array of ints.
-	static int[] statArray; // Used to arrange stats more neatly.
+	static double[][] statArray = new double[7][4]; // Used to arrange stats
+													// more neatly.
+	static int[] pratt; // Set to hold the values of Pratt's sequence.
+	static int LENGTH = 142; // This is length of pratt array.
 	static int size;
 	static int repCount = 10;
 	static int swapCount;
@@ -12,29 +15,59 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		prattInterval();
+		// Prints label to know order of stats.
+		System.out.println("size \t Bubble Swaps \t Bubble Compare \t Bubble time "
+				+ "\t Selection Swaps \t Selection Compares \t Selection Time "
+				+ "\t Insertion Swaps \t Insertion Compares \t Insertion time "
+				+ "\t Quick Swaps \t Quick Compares \t Quick Time "
+				+ "\t Hibbard Swaps \t Hibbard Compares \t Hibbard Time "
+				+ "\t Knuth Swaps \t Knuth Compares \t Knuth time " + "\t Pratt Swaps \t Pratt Compares \t Pratt time");
 		for (size = 100; size <= 20000; size += 100)
 		{
-			numArray = new int[size];
-			for (int i = 0; i < size; i++)
-			{
-				numArray[i] = (int) (Math.random() * 1000000);
-				// System.out.println("index: " + i + " is " + numArray[i]);
-			}
-			for (int i = 1; i <= repCount; i++)
-			{
-				// make call to sorting method here (just remove //)
-				// bubbleSort();
-				// insertionSort();
-				// selectionSort();
-				quickSort(0, size - 1);
+			// Sets stats to zero because the array only
+			// holds one size worth of stats.
+			initStatArray();
 
-				// shellSort();
-				// TODO Auto-generated method stub
+			// Runs through the sorts for amount of reps needed.
+			for (int currentRep = 1; currentRep < repCount; currentRep++)
+			{
+				initValues(); // Creates array
+				bubbleSort();
+				storeStat(0); // Stores the stats for bubble
+				statClear(); // Resets counters
+				initValues();
+				selectionSort();
+				storeStat(1); // Stores the stats for selection
+				statClear();
+				initValues();
+				insertionSort();
+				storeStat(2); // Stores the stats for insertion
+				statClear();
 			}
-			System.out.println(compareCount / repCount);
-			statClear();
-
+			// This loop increases the rep count for the faster sorts.
+			for (int currentRep = 1; currentRep < repCount * 100; currentRep++)
+			{
+				initValues();
+				quickS(0, size - 1);
+				storeStat(3); // Stores stats for Quick Sort
+				statClear();
+				initValues();
+				shellSort(hibbardsinterval(), 2);
+				storeStat(4); // Stores stats for Hibbard's Shell sort
+				statClear();
+				initValues();
+				shellSort(knuthsInterval(), 3);
+				storeStat(5); // Stores stats for Knuth's Shell sort
+				statClear();
+				//initValues();
+				//prattSort();
+				//storeStat(6);
+				//statClear();
+			}
+			printStat();
 		}
+
 	}
 
 	static public boolean isSorted()
@@ -135,84 +168,55 @@ public class Main
 	{
 		int indexOfMin = startIndex;
 		for (int index = startIndex + 1; index <= endIndex; index++)
+		{
 			if (numArray[index] < numArray[indexOfMin])
 				indexOfMin = index;
-		compareCount++;
+			compareCount++;
+		}
 		return indexOfMin;
 	}
 
-	static int split(int first, int last)
-	 {
-		 
-	   int splitVal = numArray[first];
-	   int saveF = first;
-	   boolean onCorrectSide;
-	 
-	   first++;
-	   do
-	   {
-	     onCorrectSide = true;
-	     while (onCorrectSide)             // move first toward last
-	     {
-	   	  compareCount++;
-	   	  if (numArray[first] > splitVal)	
-	   		  onCorrectSide = false;
-	         else
-	         {
-	       	  first++;
-	       	  onCorrectSide = (first <= last);
-	         }
-	     }//End of while loop
+	static void quickS(int first, int last)
+	{
+		double startTime = System.currentTimeMillis();
 
-	     onCorrectSide = (first <= last);
-	     while (onCorrectSide)             // move last toward first
-	     {
-	   	  compareCount++;
-	   	  if (numArray[last] <= splitVal)
-	   		  onCorrectSide = false;
-	         else
-	         {
-	       	  last--;
-	       	  onCorrectSide = (first <= last);
-	         }
-	     }//End of while loop
-	     
-	     if (first < last)                
-	     {
-	       swap(first, last);
-	       first++;
-	       last--;
-	     }
-	   } //End of do while.
-	   while (first <= last);
-	   	swap(saveF, last);
-	   return last;
-	 }//End of split
+		if (first < last)
+		{
+			int i = first;		//Start at the two ends
+			int j = last + 1;	//of the list.
+			int x = numArray[first]; // This is the pivot value
+			do
+			{
+				do		// Moving left
+				{
+					i++;
+					compareCount++;
+				} while (numArray[i] < x && i < size - 1);
 
-	 static void quickSort(int first, int last)
-	 {
-	   if (first < last)
-	   {
-	     int splitPoint;
-	 
-	     splitPoint = split(first, last);
-	     // values[first]..values[splitPoint - 1] <= splitVal
-	     // values[splitPoint] = splitVal
-	     // values[splitPoint+1]..values[last] > splitVal
-	 
-	     quickSort(first, splitPoint - 1);
-	     quickSort(splitPoint + 1, last);
-	   }
-	 }
+				do		// Moving right
+				{
+					j--;
+					compareCount++;
+				} while (numArray[j] > x && j > first);
 
+				if (i < j)
+					swap(i, j);
+				else
+					break;
 
-	/*
-	 * static void shellSort(int num[],int N, int interval[]) { p = 0; do { k =
-	 * interval[p]; for(i = 0; i < (N - k); i++) { for } } }
-	 */
+			} while (true);
+			swap(first, j);
+			quickS(first, j - 1); //Recursively call each side
+			quickS(j + 1, last);  //of the list
+			time += System.currentTimeMillis() - startTime;
+
+		}
+	}
 
 	static void print(int size)
 	{
+		// This method is used to simply print the entire
+		// array to confirm that it is being sorted.
 		for (int i = 0; i < size; i++)
 		{
 			System.out.println(numArray[i]);
@@ -226,4 +230,155 @@ public class Main
 		time = 0;
 	}
 
+	static void initValues()
+	// Initializes the values array with random integers from 0 to 999999.
+	{
+		numArray = new int[size];
+		for (int index = 0; index < size; index++)
+			numArray[index] = (int) (Math.random() * 1000000);
+	}
+
+	static void shellSort(int a, int b)
+	// Sorts items in the array using interval 
+	// passed to it in the header.
+	{
+		double startTime = System.currentTimeMillis();
+		int interval = a;
+		do
+		{
+			for (int i = 0; i < (size - interval); i++)
+			{
+				for (int j = i; j >= 0; j -= interval)
+				{
+					if (numArray[j] <= numArray[j + interval])
+					{
+						compareCount++;
+						break;
+					} else
+						swap(j, j + interval);
+				}
+			}
+			interval /= b; // Divides the interval by number passed
+							// in the header.
+		} while (interval > 0);
+
+		time += (System.currentTimeMillis() - startTime);
+
+	}
+
+	static int hibbardsinterval()
+	{
+		int d = 1;
+		while (d < size)
+			d *= 2;
+		d -= 1;
+		d /= 2;
+		return d;
+	}
+
+	static int knuthsInterval()
+	{
+		int d = 1;
+		while (d < size)
+		{
+			d *= 3;
+			d += 1;
+		}
+		d /= 3;
+		return d;
+	}
+
+	static void prattInterval()
+	{
+		int i, last2ind = 0, last3ind = 0;
+		pratt = new int[LENGTH];
+
+		pratt[0] = 1;
+		for (i = 1; i < LENGTH; ++i)
+		{
+			if (pratt[last2ind] * 2 < pratt[last3ind] * 3)
+			{
+				pratt[i] = pratt[last2ind] * 2;
+				last2ind++;
+			} else if (pratt[last2ind] * 2 > pratt[last3ind] * 3)
+			{
+				pratt[i] = pratt[last3ind] * 3;
+				last3ind++;
+			} else
+			{
+				/* avoid repeats: don't output the same # twice! */
+				pratt[i] = pratt[last2ind] * 2;
+				last2ind++;
+				last3ind++;
+			}
+		}
+	}
+
+	static void prattSort()
+	{
+		
+		double startTime = System.currentTimeMillis();
+		int interval = 1;
+		while (pratt[interval] < size)
+			interval++;
+		interval--;
+		// p = 0;
+		do
+		{
+
+			for (int i = 0; i < (size - pratt[interval]); i++)
+			{
+				for (int j = i; j >= 0; j -= pratt[interval])
+				{
+					if (numArray[j] <= numArray[j + pratt[interval]])
+					{
+						compareCount++;
+						break;
+					} else
+						swap(j, j + pratt[interval]);
+				}
+			}
+			interval--;
+		} while (interval > 0);
+
+		time += (System.currentTimeMillis() - startTime);
+
+	}
+
+	static void printStat()
+	{
+		System.out.print("\n" + size + "\t");
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 1; j < 4; j++)
+			{
+				if (i > 2) // Then it is a faster sort and uses more reps.
+					System.out.print(statArray[i][j] / (repCount * 100) + "\t");
+				else
+					System.out.print(statArray[i][j] / repCount + "\t");
+
+			}
+
+		}
+	}
+
+	static void storeStat(int sort)
+	{
+		statArray[sort][0] = size;
+		statArray[sort][1] += swapCount;
+		statArray[sort][2] += compareCount;
+		statArray[sort][3] += time;
+
+	}
+
+	static void initStatArray()
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				statArray[i][j] = 0;
+			}
+		}
+	}
 }
